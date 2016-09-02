8393ba235720d8186c6304193fff0f950a45c8af
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2016-09-02 19:07:18 +0200

    Fix (plurals with adjectives), documentation

--- expand_surface_forms.py
+++ expand_surface_forms.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
+Expanding surface forms into all cases. Use this on a JSON of slots, values, and possible
+surface form lemmas. The output must be postprocessed by hand (as morphological dictionary
+is lacking on restaurant names and similar).
+
+Czech-specific + SDS-specific, partially domain-specific (in terms of which inflection forms
+are produced)
 """
+
 from __future__ import unicode_literals
 
 import itertools
@@ -28,6 +35,11 @@ class ExpandSurfaceForms(object):
         self._generator = Generator(generator_dict)
 
     def process_file(self, input_fname, output_fname):
+        """Expand surface forms in one JSON file.
+
+        @param input_fname: input JSON file (slots -> values -> possible surface form lemmas)
+        @param output_fname: output JSON file (slots -> values -> surface forms in all cases)
+        """
         # read input
         with codecs.open(input_fname, 'rb', 'UTF-8') as fh:
             data = json.load(fh)
@@ -40,13 +52,18 @@ class ExpandSurfaceForms(object):
                     # analyze the word
                     words = self._analyzer.analyze(variant)
                     # ganther required inflection forms
+                    # verbs: 2nd person present + infinitive
                     if words[0][2].startswith('V'):
-                        infls = [{'person': '2'}, {'person': '-'}] # keep infinitive as well
+                        infls = [{'person': '2'}, {'person': '-'}]
+                    # nouns/adjectives: all cases (except vocative)
                     else:
                         infls = [{'case': '1'}, {'case': '2'}, {'case': '3'},
                                  {'case': '4'}, {'case': '6'}, {'case': '7'}]
-                        # all numbers for some cases (domain specific)
-                        if re.search(r'(^| )(snídaně|oběd|večeře|brunch)( |$)', variant):
+                        adj_adv = (slot != 'street' and
+                                   all(re.match('^[AD]', word[2]) for word in words))
+                        # all numbers for some cases (domain specific) and adjective/adverb
+                        if (adj_adv or
+                                re.search(r'(^| )(snídaně|oběd|večeře|brunch)( |$)', variant)):
                             new_infls = []
                             for infl in infls:
                                 for number in ['S', 'P']:
@@ -55,7 +72,7 @@ class ExpandSurfaceForms(object):
                                     new_infls.append(new_infl)
                             infls = new_infls
                         # all genders for adjective/adverb only (except street names)
-                        if slot != 'street' and all(re.match('^[AD]', word[2]) for word in words):
+                        if adj_adv:
                             new_infls = []
                             for infl in infls:
                                 for gender in ['M', 'I', 'F', 'N']:
@@ -80,6 +97,7 @@ class ExpandSurfaceForms(object):
             json.dump(data, fh, ensure_ascii=False, indent=4)
 
     def get_main_tag(self, tags):
+        """Given a NE, get the main tag (typically a noun)"""
         # TODO better handling of "U Konšelů" and similar
         if all(re.match(r'^[NA]', t) for t in tags):
             tag = next(iter(reversed([t for t in tags if re.match(r'....[2-7]', t)])), None)
@@ -96,6 +114,7 @@ class ExpandSurfaceForms(object):
             if tag:
                 return tag
 
+        # default to first noun
         return tags[0]
 
 
